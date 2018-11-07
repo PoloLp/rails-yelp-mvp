b79f1bbfbc330b1090db30d5c7974e73d28ce215
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-07 13:44:50 +0100

    save model validations

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,13 @@
+class RestaurantsController < ApplicationController
+
+ def show
+ end
+
+ def new
+
+ end
+
+ def index
+
+ end
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,16 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true,
+                       :inclusion => { :in => ['chinese',
+                                               'italian',
+                                               'japanese',
+                                               'french',
+                                               'belgian'] }
+
 end
+
+# The restaurant category
+# should belong to a fixed list ["chinese", "italian", "japanese", "french", "belgian"].
