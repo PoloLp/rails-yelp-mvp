5f3d36839f3d2ed5c574771d20a44edb850a546b
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-07 13:45:56 +0100

    save model validations2

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
 
-  validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true },
+                     inclusion: { in: 0..5 }
 end
